ca619956ca708ccde3fb567ae87b83be81b04481
kojiro-hara <jirou884989@gmail.com>
2025-03-21 13:42:33 +0900

    バリデーション定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/,}
+  validates :family_name, presence: true
+  validates :first_name, presence: true
+  validates :family_reading, presence: true
+  validates :first_reading, presence: true
+  validates :birthday, presence: true
 end
