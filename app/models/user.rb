ab1372a6b641cff27220f1875af6d3bc1003972d
kojiro-hara <jirou884989@gmail.com>
2025-03-31 09:46:18 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/ }
-  validates :family_name, presence: true
-  validates :first_name, presence: true
+  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/ }
+  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   validates :family_reading, presence: true, format: { with: /\A[\p{katakana}\u30A0-\u30FF]+\z/ }
   validates :first_reading, presence: true, format: { with: /\A[\p{katakana}\u30A0-\u30FF]+\z/ }
   validates :birthday, presence: true
