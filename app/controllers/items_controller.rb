c102b7bf6daf4b7265ebc8f1299ec615b2c0bc8e
kojiro-hara <jirou884989@gmail.com>
2025-04-03 15:37:53 +0900

    商品画像添付

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,5 +9,18 @@ class ItemsController < ApplicationController
   end
   
   def create
+    @item = Item.new(item_params)
+    @item.user = current_user
+
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:product_name, :text, :price, :category_id, :state_id, :burden_id, :prefecture_id, :days_id, :image)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,15 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
+
+  validates :product_name, presence: true, length: { maximum: 40 }
+  validates :text, presence: true, length: { maximum: 1000 }
+  validates :category_id, presence: true
+  validates :state_id, presence: true
+  validates :burden_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :days_id, presence: true
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+
 end
