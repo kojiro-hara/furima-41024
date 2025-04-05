b6c4ce2e99760e4c34487e2ad1d7a684e3cd4a35
kojiro-hara <jirou884989@gmail.com>
2025-04-05 13:58:48 +0900

    エラーメッセージ表示

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('turbo:load', () => {
   const priceInput = document.getElementById("item-price");
   if (!priceInput) return; // フォームが存在しないページでは何もしない
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -10,13 +10,14 @@ class Item < ApplicationRecord
   belongs_to :days
 
 
+  validates :image, presence: true
   validates :product_name, presence: true, length: { maximum: 40 }
   validates :text, presence: true, length: { maximum: 1000 }
-  validates :category_id, presence: true
-  validates :state_id, presence: true
-  validates :burden_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :days_id, presence: true
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank"  }
+  validates :state_id, numericality: { other_than: 1, message: "can't be blank"  }
+  validates :burden_id, numericality: { other_than: 1, message: "can't be blank"  }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank"  }
+  validates :days_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
 end
