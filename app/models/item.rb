dcc81c8d3fc2792b61176b15136a356ec3c39553
kojiro-hara <jirou884989@gmail.com>
2025-04-07 15:56:24 +0900

    テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,7 +13,7 @@ class Item < ApplicationRecord
   validates :image, presence: true
   validates :product_name, presence: true, length: { maximum: 40 }
   validates :text, presence: true, length: { maximum: 1000 }
-  validates :category_id, numericality: { other_than: 1, message: "can't be blank"  }
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank"  } 
   validates :state_id, numericality: { other_than: 1, message: "can't be blank"  }
   validates :burden_id, numericality: { other_than: 1, message: "can't be blank"  }
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank"  }
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :item do
-    
+    product_name { "Test Product" }
+    text { "Test description" }
+    category_id { 2 }  # 必要なIDを設定（category_idが1以外）
+    state_id { 2 }     # state_idも設定（state_idが1以外）
+    burden_id { 2 }    # burden_idも設定（burden_idが1以外）
+    prefecture_id { 2 }  # prefecture_idも設定（prefecture_idが1以外）
+    days_id { 2 }      # days_idも設定（days_idが1以外）
+    price { 500 }      # 価格（300以上、10000000以下）
+    association :user  # Userを関連付け
+
+     # 画像の添付
+     image { Rack::Test::UploadedFile.new(Rails.root.join('spec', 'fixtures', 'files', '蚊.png'), 'image/png') }
   end
 end
