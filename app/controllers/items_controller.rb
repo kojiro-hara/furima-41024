b40f474286327e39d9d1ce7e059345be7a582d85
kojiro-hara <jirou884989@gmail.com>
2025-04-14 14:14:36 +0900

    コメントアウト削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,9 @@
 class ItemsController < ApplicationController
-  # ログインユーザーしか new/create/edit/update を使えないようにする
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  # 該当商品の情報を取得（edit, update, show などで使う）
   before_action :set_item, only: [:edit, :update, :show]
-  # 出品者以外が編集ページにアクセスしたらトップにリダイレクト
   before_action :redirect_unless_owner, only: [:edit, :update]
-  # セレクトボックス用データの取得
   before_action :set_select_data, only: [:edit, :update]
 
-  
-  
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -32,11 +26,11 @@ class ItemsController < ApplicationController
   end
 
   def show
-  # set_item が呼ばれているのでここでは何も追加不要
+
   end
 
   def edit
-   # set_item と redirect_unless_owner が事前に呼ばれている
+
   end
   
   def update
@@ -49,24 +43,20 @@ class ItemsController < ApplicationController
 
   private
 
-  # Strong Parameters
   def item_params
     params.require(:item).permit(:product_name, :text, :price, :category_id, :state_id, :burden_id, :prefecture_id, :days_id, :image)
   end
 
-  # 商品を取得
   def set_item
     @item = Item.find(params[:id])
   end
 
-  # 出品者でなければトップページにリダイレクト
   def redirect_unless_owner
     unless @item.user_id == current_user.id
       redirect_to root_path
     end
   end
 
-  # セレクトボックス用データの読み込み
   def set_select_data
     @categories = Category.all
     @states = State.all
